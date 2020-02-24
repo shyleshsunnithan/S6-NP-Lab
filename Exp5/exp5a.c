47928141c3240a9da3a9e4c9e011ffa91bf59a94
Shylesh S <37534898+shyleshsunnithan@users.noreply.github.com>
2020-02-24 23:51:14 +0530

    Update exp5a.c

--- Exp5/exp5a.c
+++ Exp5/exp5a.c
@@ -8,7 +8,7 @@ pthread_t idAdd;
 
 void* add(void *arg) {
 	int a, b;
-	printf("Enter two numbers : ");
+	printf("Enter Two Numbers : ");
 	scanf("%d %d", &a, &b);
 	printf("Sum = %d\n", a+b);
 }
@@ -17,15 +17,15 @@ int main() {
 	int i=0, err;
 	err = pthread_create(&idAdd, NULL, &add, NULL);
 	if (err != 0) {
-		printf("Cant create thread : [%s]\n", strerror(err));
+		printf("Cant create thread now : [%s]\n", strerror(err));
 	} else {
-		printf("Thread created successfully\n");
+		printf("Thread has been created successfully\n");
 	}
 	err = pthread_join(idAdd, NULL);
 	if (err != 0) {
 		printf("Cant joining thread : [%s]\n", strerror(err));
 	} else {
-		printf("Thread finished successfully\n");
+		printf("Thread has been finished successfully\n");
 	}
 	return 0;
 }
